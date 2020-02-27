30f4c7bbbaeb31692fcda8ddf0bfaea84f32626d
swetank01 <iamswetanksoni@gmail.com>
2020-02-27 13:19:14 +0530

    google vscode

--- install-test.ps1
+++ install-test.ps1
@@ -1,3 +1,4 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install reportviewer2012 dotnet4.0 -y
+choco install reportviewer2012 dotnet4.0 google-chrome vscode -y
+
