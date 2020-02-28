30856c0e6e5463fec3508cce22eb6393f488ccce
unknown <shyamjangid178@gmail.com>
2020-02-28 11:34:22 +0530

    update cmdlet

--- testscrip.ps1
+++ testscrip.ps1
@@ -5,6 +5,6 @@
 # Install-Module -Name SqlServer -Force AllowClover
 
 #Run SQL 
-Invoke-Sqlcmd  -Database master -Query "CREATE LOGIN wsustest2 WITH PASSWORD = 'a@nonymous@2020!';" -ServerInstance localhost
-
+#Invoke-Sqlcmd  -Database master -Query "CREATE LOGIN wsustest2 WITH PASSWORD = 'a@nonymous@2020!';" -ServerInstance localhost
+Get-Help update
 #install chrome
